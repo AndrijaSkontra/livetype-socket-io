3736311716b6b2bde371e2d5d73ee1865127dbbf
AndrijaSkontra <andrija.skontra@kodelab.hr>
2025-02-18 21:00:02 +0100

    keep track of connected usernames

--- index.js
+++ index.js
@@ -18,8 +18,23 @@ const io = new Server(server, {
   },
 });
 
+const connectedUsernames = new Set();
+
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  const username = socket.handshake.query.username;
+  if (connectedUsernames.has(username)) {
+    console.log("Already connected go away son");
+    socket.disconnect();
+  } else {
+    connectedUsernames.add(username);
+    console.log("username connected: ", username);
+    // console log all socket ids
+    //const connectedSocketIds = Array.from(io.sockets.sockets.keys());
+    //console.log("Connected socket IDs:", connectedSocketIds);
+    if (connectedUsernames.size === 4) {
+      io.emit("start01");
+    }
+  }
 });
 
 server.listen(3001, () => {
